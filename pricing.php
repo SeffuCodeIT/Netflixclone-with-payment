33a790c1986c50000dc2b16b27b062d496e7ca39
Fortune <fortunelangat54@gmail.com>
2022-03-10 09:51:01 +0300

    chaange

--- pricing.php
+++ pricing.php
@@ -94,7 +94,6 @@ if(isset($_POST['premium'])){
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
     <link rel="stylesheet" href="form.css">
 </head>
 <body>
